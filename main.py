1c91f273f0f19f69c1040d5aac63a7f45deb34a0
aristotelesfl <fl.aristoteles@gmail.com>
2023-09-08 12:33:20 -0300

    implementação da função geradora de saídas, gaeradora de matríz e finalização do projeto

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
 all_gates = ["AND", "NAND", "NOT", "OR", "NOR", "XOR", "XNOR"]
 output_file = "Tabela.txt"
 circuit = {
-        "entradas": [],
+        "entradas": {},
         "gates": {}
     }
  
+#Gera as entradas do circuito
 def inputs(n: int)->list:
     all_inputs = []
     for i in range(2**n):
@@ -13,16 +13,24 @@ def inputs(n: int)->list:
         all_inputs.append([int(bit) for bit in binario])
     return all_inputs
 
-def logical(gate: str, a:bool, b:bool)->bool:
-    if gate == "AND":
+#Cria uma sublista com as saídas dos gates
+def list_outputs(dicio: dict)->list:
+    out = []
+    for value in dicio["gates"]:
+        out.append(dicio['gates'][value][1])
+    return out
+
+#Faz as operações lógicas
+def logical(gate: str, a:bool, b:bool)->int:
+    if gate == "AND" or gate == "NAND":
         return a and b
-    elif gate == "OR":
+    elif gate == "OR" or gate == "NOR":
         return a or b
-    elif gate == "XOR":
+    elif gate == "XOR" or gate == "XNOR":
         return a ^ b
 
-def gate_not(gate, a: bool)->bool:
-    if gate == "NOT":
+#Faz o complemento das operações lógicas
+def gate_not(a: bool)->int:
         return not(a)
 
 #Encontra os gates e separa numa sublista
@@ -75,21 +83,98 @@ def sliceContent_toDict(content: str, circuit: dict)->None:
     index_input = content.index('ENTRADAS')
     index_output = content.index('SAIDAS')
     index_gate = content.index('GATES')
-    for i in content[index_input+1:index_output]:
-        circuit["entradas"].append(i)
-    
+    keys = content[index_input+1:index_output]
+    gate_input = inputs(len(keys))
+    for i in keys:
+        value = []
+        key, index = i, keys.index(i)
+        for j in gate_input:
+            value.append(j[index])
+        circuit['entradas'][key] = value
+    outputs = []
     for i in content[index_output+1:index_gate]:
-        circuit["saidas"] = i
+        outputs.append(i)
+    circuit["saidas"] = outputs
     
     gates = list_gates(content, all_gates)
-    content = content[index_gate+len(gates):]
+    content = content[index_gate:]
     sliceContent_byGates (content, gates)
 
+#Gera as saídas dos gates
+def generate_outputs(dicio: dict)->None:
+    lista = list_outputs(dicio)
+    outputs = dicio["saidas"]
+    dicio["saidas"] = {}
+    for key in dicio['gates']:
+        values, output = [], []
+        gate, out = dicio['gates'][key][0], dicio['gates'][key][1]
+        for value in dicio['gates'][key]:
+            index = dicio['gates'][key].index(value)
+            if value in dicio['entradas']:
+                values.append(dicio['entradas'][value])
+            elif index!=1 and value in lista:
+                i = lista.index(value)
+                current_list = list_outputs(dicio)
+                values.append(current_list[i][value])
+        
+        if gate == "NOT":
+            a = values
+            for l in range(len(a[0])):
+                output.append(int(gate_not(a[0][l])))
+
+        else:
+            cout = False
+            for i in range(len(values)-1):
+                if cout is False:
+                    a, b = values[0], values[1]
+                    for l in range(len(a)):
+                        output.append(int(logical(gate, a[l], b[l])))
+                    cout = True
+                else:
+                    a, b = output, values[i+1]
+                    output = []
+                    for l in range(len(a)):
+                        output.append(int(logical(gate, a[l], b[l])))
+            if gate == "NOR" or gate == "XNOR" or gate == "NAND":
+                current_output = []
+                for i in output:
+                    current_output.append(int(gate_not(i)))
+                output = current_output
+
+        dicio['gates'][key][1] = {
+            out: output
+        }
+
+        if out in outputs:
+            dicio['saidas'][out] = dicio['gates'][key][1][out]
+    
+
+#Cria uma lista com as entradas e saída do circuito
 def create_table(circuit: dict)->list:
-    pass
+    matriz = []
+    for i in circuit["entradas"].values():
+        matriz.append(i)
+    for i in circuit["saidas"].values():
+        matriz.append(i)
+    return matriz
 
-string = fragment_strText(readLocal_file())
+#Imprime a tabela verdade do circuito num arquivo "Tabela.txt"
+def write_matriz(matriz:list, dicio:dict)->None:
+    table = open("Tabela.txt", "w+")
+    for index_i in dicio['entradas']:
+        table.write(f"{index_i} ")
+    for index_o in dicio['saidas']:
+        table.write(f"{index_o} ")
+    table.write("\n")
+    for i in range(len(matriz[0])):
+        for j in range(len(matriz)):
+            table.write(f"{matriz[j][i]} ")
+        table.write("\n")
+    table.close()
 
-sliceContent_toDict(string, circuit)
-
-print (circuit)
+strTXT = readLocal_file()
+content = fragment_strText(strTXT)
+sliceContent_toDict(content, circuit)
+generate_outputs(circuit)
+matriz = create_table(circuit)
+write_matriz(matriz, circuit)
